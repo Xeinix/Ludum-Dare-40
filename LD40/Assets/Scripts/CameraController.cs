f888e649f68af59c5e5f74638f6cf511a57d38c8
Zachary Seebeck <xeinix@outlook.com>
2017-12-02 14:08:45 -0500

    Initial Commit
    Already have some work done but it's minimal at best.

---
+++ LD40/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    #region Movement Vars
+
+    public double followStartX = 0.0f;
+    public double followEndX = 0.0f;
+    public GameObject player;
+    public GameObject cam;
+
+    #endregion
+
+    private void Start()
+    {
+        Debug.Log("CameraController started.");
+    }
+
+    private void Update()
+    {
+
+
+        if (player.transform.position.x > followStartX && cam.transform.position.x <= followEndX )
+        {
+            cam.transform.position = new Vector3(player.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        }
+    }
+
+}
---
+++ LD40/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+    
+
+
+    #region Player vars
+
+    public float moveForceY = 0.5f;
+    public float moveForceX = 0.5f;
+    public float charWeight = 5;
+
+    public Rigidbody2D rb;
+
+    #endregion
+
+    void Start () {
+        Debug.Log("Player Controller Loaded.");
+	}
+	
+	void FixedUpdate () {
+
+        rb.mass = charWeight;
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Move Character Up.");
+            Vector2 vec2 = new Vector2(0, moveForceY);
+
+            rb.velocity = vec2;
+        }
+        else
+        {
+            Vector2 vec2 = new Vector2(moveForceX, rb.velocity.y);
+            rb.velocity = vec2;
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "nuggGold")
+        {
+            Debug.Log("Add in nugget pickup.");
+        }
+        if (collision.gameObject.tag == "nuggBlack")
+        {
+            Debug.Log("Add in nugget pickup.");
+        }
+    }
+}
